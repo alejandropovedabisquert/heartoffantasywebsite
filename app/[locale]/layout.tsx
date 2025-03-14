b05101be3957e88794a07cb42220932cbf62d526
Alejandro Poveda Bisquert <alejandropovedabisquert@gmail.com>
2025-03-14 17:09:04 +0100

    Update styles and functionality switch languages

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -47,7 +47,7 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang={locale} className="scroll-smooth">
       <AOSInit />
       <body
         className={`${geistSans.className} antialiased`}
--- components/common/LocaleSwitcherSelect.tsx
+++ components/common/LocaleSwitcherSelect.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { startTransition } from 'react';
+import { startTransition, useEffect, useRef, useState } from 'react';
 import { useLocale, useTranslations } from 'next-intl';
-import { usePathname, useRouter } from '@/i18n/navigation';
+import { Link, usePathname, useRouter } from '@/i18n/navigation';
 import { useParams } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import { Globe } from 'lucide-react';
+import clsx from 'clsx';
 
 interface languagesProps {
   locale: string;
@@ -11,13 +14,14 @@ interface languagesProps {
 }
 
 export default function LanguageSwitcher() {
-  const t = useTranslations();
-  const languages = t.raw("LanguageSwitcher")
+  const [open, isOpen] = useState<boolean>(false);
+  const heightRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState<number | undefined>(0);
+  const t = useTranslations("LocaleSwitcher");
   const router = useRouter();
   const pathname = usePathname();
   const params = useParams();
   const currentLocale = useLocale();
-
   const switchLanguage = (nextLocale: string) => {
     if (nextLocale === currentLocale) return;
 
@@ -30,17 +34,40 @@ export default function LanguageSwitcher() {
         { locale: nextLocale } // Cambia el idioma
       );
     });
+    router.refresh();
   };
+  useEffect(()=>{
+    setHeight(heightRef.current?.scrollHeight)
+  });
+
+  const heightStyle = clsx(
+    open ? `h-[${height}px]` : "h-0"
+  );
+  
+  
 
   return (
-    <div className="absolute right-8 top-8 z-10">
-      {
-        languages.map((language: languagesProps, index: number) => (
-          <button key={index} onClick={() => switchLanguage(`${language.locale}`)} hidden={currentLocale === language.locale}>
-            {language.language}
-          </button>
-        ))
-      }
+    <div className="absolute left-8 top-8 z-10">
+      <div className='flex gap-2 items-center justify-center bg-[#a43045] p-2 cursor-pointer transition-all hover:bg-white hover:text-black' onClick={ ()=> isOpen(!open) }>
+        <Globe/>
+        {t('locale', { locale: currentLocale })}
+      </div>
+      <div ref={heightRef} className={`text-center overflow-hidden bg-[#A43046] transition-all ${heightStyle}`}>
+        {routing.locales.map((cur) => (
+          <>
+          {
+            cur != currentLocale ? (
+              <a href={cur} hidden={cur == currentLocale} className='p-2 block w-full bg-[#A43046] transition-all hover:bg-white hover:text-black'>
+                {t('locale', { locale: cur })}
+              </a>
+            ):null
+          }
+          </>
+          // <button key={cur} onClick={() => switchLanguage(cur)} hidden={cur == currentLocale} className='p-2 w-full'>
+          //   {t('locale', { locale: cur })}
+          // </button>
+        ))}
+      </div>
     </div>
   );
 }
