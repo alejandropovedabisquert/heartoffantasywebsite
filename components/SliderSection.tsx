dcd84b71300d533485927dcc4eb69c5c88b9b861
Alejandro Poveda <alejandropovedabisquert@gmail.com>
2025-10-03 21:45:38 +0200

    Change height initial slider

--- components/SliderSection.tsx
+++ components/SliderSection.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 // TODO: Ajustar altura del video para hacerlo mas usable para usuarios y ponerle minimo de altura
 export default function SliderSection() {
       const gradientStyles = clsx(
-        "before:bg-gradient-to-b before:absolute before:top-0 before:h-screen before:max-h-[60vh] before:w-full before:from-black before:to-transparent before:z-[1]",
-        "after:bg-gradient-to-t after:absolute after:bottom-0 after:h-screen after:max-h-[60vh] after:w-full after:from-black after:to-transparent after:z-[1]",
+        "before:bg-gradient-to-b before:absolute before:top-0 before:h-screen before:max-h-[40vh] before:w-full before:from-black before:to-transparent before:z-[1]",
+        "after:bg-gradient-to-t after:absolute after:bottom-0 after:h-screen after:max-h-[40vh] after:w-full after:from-black after:to-transparent after:z-[1]",
       );
     return (
-        <div className={`h-screen ${gradientStyles}`}>
-            <div className="absolute top-0 h-screen w-full bg-[url(/bg_principal.jpg)] bg-center bg-cover bg-no-repeat" />
+        <div className={`min-h-[500px] relative ${gradientStyles}`}>
+            <div className="absolute top-0 min-h-[500px] w-full bg-[url(/bg_principal.jpg)] bg-center bg-cover bg-no-repeat" />
             <div className="absolute left-1/2 top-1/2 ease-in -translate-y-1/2 -translate-x-1/2 z-10">
-                <div className="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px]">
+                <div className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px]">
                     <Image src={"/logo.png"} fill alt="logo" priority={true} />
                 </div>
             </div>
             <video
-                className="absolute top-0 w-full h-screen object-cover"
+                className="absolute top-0 w-full h-[500px] object-cover"
                 preload="none" aria-label="Video player" autoPlay controls={false} loop muted playsInline
             >
                 <source src={"/bg_video.mp4"} type="video/mp4" />
