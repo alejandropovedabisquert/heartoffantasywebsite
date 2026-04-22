94b66627d84963e4e77cebcc61ecf23ffac99cac
Alejandro <alejandropovedabisquert@gmail.com>
2026-04-22 19:34:37 +0200

    Revert "fix trustline prod"
    This reverts commit 66fa13ba377eac9b46017bf7bf63435c97214d3f.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -55,7 +55,8 @@ export default async function RootLayout({
     <html lang={locale} suppressHydrationWarning className='overflow-x-hidden'>
       <head>
         <Script
-          src='https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit'
+          src='https://challenges.cloudflare.com/turnstile/v0/api.js'
+          async
           defer
         />
       </head>
